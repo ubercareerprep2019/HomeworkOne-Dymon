7ee0e0c0d53d8b0189e8e8d4ec04e1cbd6f8d55a
Dymon Moore <dmoore12@villanova.edu>
2019-05-15 22:34:22 -0400

    Stack file, methods without implementation.

--- src/Stack.java
+++ src/Stack.java
@@ -1,4 +1,32 @@
-
 public class Stack {
 
+	int integer = 0;
+	int[] data;
+	
+	
+	public Stack()
+	{
+		
+	}
+	
+	public void push(int x)
+	{
+		
+	}
+	
+	public int pop()
+	{
+		return 0;
+	}
+	
+	public int top()
+	{
+		return 0;
+	}
+	
+	public boolean isEmpty()
+	{
+		return false;
+	}
 }
+
