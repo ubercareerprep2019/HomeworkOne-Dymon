e7942db49564b890c2720cda4d206b0726749920
Dymon Moore <dmoore12@villanova.edu>
2019-05-15 22:12:27 -0400

    Stack file. Method implementation needed.

---
+++ src/Stack.java
@@ -0,0 +1,42 @@
+public class Stack {
+
+	int number;
+	int [] data;
+	
+//	Stack test = new Stack();
+	
+	public Stack()
+	{
+		
+	}
+
+	
+	public void push(int number)
+	{
+		
+	
+	}
+	
+	public int pop()
+	{
+		return 0;
+	}
+	
+	public int top()
+	{
+		return 0;
+	}
+	
+	public boolean isEmpty() {
+		
+		return false;
+	}
+	public static void main(String [] args)
+	{
+		Stack test = new Stack();
+		
+		test.push(1);
+		
+	}
+	
+}
