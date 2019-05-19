e66910f17610af85e11c56c60cbaee3398e640a2
Dymon Moore <dmoore12@villanova.edu>
2019-05-19 10:05:13 -0400

    Cleaned up code.

--- src/QueueStack.java
+++ src/QueueStack.java
@@ -32,22 +32,10 @@ public class QueueStack {
 		QueueStack myQueue = new QueueStack();
 		
 		myQueue.enqueue(5);
-//		System.out.println(myQueue.queue.dataStore.get(0));
-		myQueue.enqueue(6);
-//		System.out.println(myQueue.queue.dataStore.get(0));
-//		System.out.println(myQueue.queue.dataStore.get(1));
-		myQueue.enqueue(7);
-//		System.out.println(myQueue.queue.dataStore.get(0));
-//		System.out.println(myQueue.queue.dataStore.get(1));
-//		System.out.println(myQueue.queue.dataStore.get(2));
-		
+		myQueue.enqueue(6);		
+		myQueue.enqueue(7);	
 		myQueue.enqueue(8);
-//		System.out.println(myQueue.queue.dataStore.get(0));
-//		System.out.println(myQueue.queue.dataStore.get(1));
-//		System.out.println(myQueue.queue.dataStore.get(2));
-//		System.out.println(myQueue.queue.dataStore.get(3));
-		
-		System.out.println(myQueue.queue.dataStore.get(0));
+
 		myQueue.dequeue();
 		myQueue.dequeue();
 		myQueue.dequeue();
